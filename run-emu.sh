bd22c956c636995a62bb4db03176d3661aed1c3f
demiban <d_icebug@hotmail.com>
2017-05-02 14:07:13 -0400

    Using raspberry pi 2 qemu model for testing emulator

--- run-emu.sh
+++ run-emu.sh
@@ -2,12 +2,11 @@
 
 IMG_DIR=buildroot/output/images
 
-utils/qemu-system-aarch64 -M raspi2 \
--cpu cortex-a7 \
+utils/qemu-system-arm \
+-M raspi2 \
 -smp 4 \
--m 1024 \
 -kernel $IMG_DIR/zImage \
--dtb $IMG_DIR/bcm2710-rpi-3-b.dtb \
 -sd $IMG_DIR/sdcard.img \
+-dtb $IMG_DIR/bcm2710-rpi-3-b.dtb \
 -serial stdio
 
